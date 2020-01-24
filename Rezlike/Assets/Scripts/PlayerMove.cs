161f9bf0e06c22434cd10af7ddef97a35346c7b1
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-01-24 22:08:13 +0000

    Player move + rails
    Player follows rails around center
    Player can move within offset of rails

--- Rezlike/Assets/Scripts/PlayerMove.cs
+++ Rezlike/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float PlayerSpeed = 0.1f; // Speed the player can move in the bounds of the offset
+    public float MaxOffset = 5f;
+
+    public Vector3 CurrentOffset = new Vector3(0f, 0f, 0f);
+
+    public Transform RailsToFollow;
+   
+    
     void Update()
     {
-        
+        // Check for offset changes
+        if (Input.GetKey(KeyCode.LeftArrow)) // X axis
+        {
+            CurrentOffset.x += PlayerSpeed * Time.deltaTime;
+
+            CurrentOffset.x = Mathf.Clamp(CurrentOffset.x, -MaxOffset, MaxOffset);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            CurrentOffset.x -= PlayerSpeed * Time.deltaTime;
+
+            CurrentOffset.x = Mathf.Clamp(CurrentOffset.x, -MaxOffset, MaxOffset);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) // Y axis
+        {
+            CurrentOffset.y += PlayerSpeed * Time.deltaTime;
+
+            CurrentOffset.y = Mathf.Clamp(CurrentOffset.y, -MaxOffset, MaxOffset);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            CurrentOffset.y -= PlayerSpeed * Time.deltaTime;
+
+            CurrentOffset.y = Mathf.Clamp(CurrentOffset.y, -MaxOffset, MaxOffset);
+        }
+
+        // Set position = rails position + offset
+        transform.position = new Vector3(RailsToFollow.position.x + CurrentOffset.x, RailsToFollow.position.y + CurrentOffset.y, RailsToFollow.position.z);
     }
 }
---
+++ Rezlike/Assets/Scripts/PlayerRails.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRails : MonoBehaviour
+{
+    public float RotateSpeed = 10f; // Speed the player rotates around center of sphere
+    public float Radius = 5f; // Distance from center
+    public float AxisSpeed = 0.5f; // Speed that axis changes    
+
+    
+    Vector3 Center = new Vector3(0f, 0f, 0f); // Center to rotate around
+    float CurrentRotation = 0f;
+
+    Vector3 AxisToRotateAround = Vector3.up;
+   
+
+
+    void Start()
+    {
+        // Set position to center + radius on x axis
+        transform.position = new Vector3(Center.x + Radius, Center.y, Center.z);
+    }
+
+    void Update()
+    {
+        // Update current rotation
+        CurrentRotation = RotateSpeed * Time.deltaTime;
+
+        // Slowly move axis
+        
+        
+        // Rotate around center based on axis
+        transform.RotateAround(Center, AxisToRotateAround, CurrentRotation);
+    }
+}
