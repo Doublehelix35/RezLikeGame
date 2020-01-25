bd4ac387b06c810f3e4c157796e483306dbfc349
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-01-25 18:04:31 +0000

    Improved orbit
    Player now orbits more organically within a set range.
    Needs to be more smooth but it works
    Improved git ignore to hope deal with library files

---
+++ Rezlike/Assets/Scripts/CenterRotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterRotate : MonoBehaviour
+{
+
+    public float RotateSpeed = 1f; // Speed that the center rotates
+    public float MaxRotation = 45f; // Max rotation in degrees
+
+    void FixedUpdate()
+    {
+        // Set random rotation percentage
+        float randX = Random.Range(0f, 1f);
+        float randZ = Random.Range(0f, 1f);
+
+        // Ping pong between -1f and 1f
+        float pingX = Mathf.PingPong(Time.time, 2f) - 1f;
+        float pingZ = Mathf.PingPong(Time.time, 2f) - 1f;
+
+        // Combine rand and ping
+        randX *= pingX;
+        randZ *= pingZ;
+
+        // Rotate center
+        transform.Rotate(randX * RotateSpeed, 0f, randZ * RotateSpeed);
+
+        // Clamp rotation
+        Vector3 clampRotation = transform.localEulerAngles;
+        clampRotation.x = Mathf.Clamp(clampRotation.x, -MaxRotation, MaxRotation);
+        clampRotation.y = 0f;
+        clampRotation.z = Mathf.Clamp(clampRotation.z, -MaxRotation, MaxRotation);
+        transform.rotation = Quaternion.Euler(clampRotation);
+    }
+}
