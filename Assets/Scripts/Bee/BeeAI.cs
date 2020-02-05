d739bf3be1fc115db49edff82976fd0e0a05dd4e
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-02-05 10:25:52 +0000

    Bee AI + Spawning
    Needs fine tuning but should work

---
+++ Assets/Scripts/Bee/BeeAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BeeAI : MonoBehaviour
+{
+    GameObject PlayerRef;
+    public float Speed = 1f;
+
+    Rigidbody Rigid;
+
+
+    void Start()
+    {
+        Rigid = GetComponent<Rigidbody>();
+        PlayerRef = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        if(PlayerRef != null)
+        {
+            // Head to player
+            Vector3 dir = PlayerRef.transform.position - transform.position;
+
+            Rigid.AddForce(dir * Speed, ForceMode.Force);
+        }
+    }
+        
+}
---
+++ Assets/Scripts/Bee/BeeSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeSpawner : MonoBehaviour
+{
+    public GameObject BeePrefab;
+
+    public int MaxBeesToSpawn = 2; // Maxium bees
+    int MinBeesToSpawn = 1;
+
+
+    bool isQuitting = false;
+
+    private void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+
+    private void OnDestroy()
+    {
+        if (!isQuitting)
+        {
+            if (BeePrefab != null)
+            {
+                int randBeesNum = Random.Range(MinBeesToSpawn, MaxBeesToSpawn + 1);
+
+                for (int i = 0; i < randBeesNum; i++)
+                {
+                    GameObject GO = Instantiate(BeePrefab, transform.position, Quaternion.identity);
+                }
+            }
+            else
+            {
+                Debug.Log("Bee prefab is null");
+            }
+        }                     
+    }
+}
