b0cb915873160867b55edf71e9211b962714525e
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-02-26 10:45:57 +0000

    Tree&Snake

--- Assets/Scripts/Snake/SnakeAi.cs
+++ Assets/Scripts/Snake/SnakeAi.cs
@@ -7,13 +7,17 @@ public class SnakeAi : MonoBehaviour
 {
     public bool IsSnakeHead = false;
 
-    public Transform PartToFollow;
-
-    public float FollowSpeed = 0.1f;
-
-    public float MinFollowDistance = 2f;
+    public Transform ObjectToFollow;
 
     Rigidbody rigid;
+
+    // Follow speeds and distances
+    public float FollowSpeed = 0.1f;
+    public float MinFollowDistance = 2f;
+    public float StopFollowDistance = 0.1f;
+
+    // Snake head only
+    public float OffsetMagnitudeMaxZ = 1f;
 
 
     void Start()
@@ -24,20 +28,42 @@ public class SnakeAi : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Calc dist
-        float distance = Vector3.Distance(PartToFollow.position, transform.position);
-        Vector3 Dir = PartToFollow.position - transform.position;
-
-        if (distance > MinFollowDistance)
-        {            
-            rigid.AddForce(Dir * FollowSpeed);
-        }
-        else
+        if (IsSnakeHead) // Follow and look at player
         {
-            rigid.AddForce(Dir * (FollowSpeed / distance));
+            // Calc dist
+            float distance = Vector3.Distance(ObjectToFollow.position, transform.position);
+            Vector3 Dir = ObjectToFollow.position - transform.position;
+
+            Dir.z = Dir.z * Mathf.PingPong(OffsetMagnitudeMaxZ, Time.time);
+
+            Dir = Dir.normalized;
+
+            if (distance > MinFollowDistance)
+            {
+                rigid.AddForce(Dir * FollowSpeed);
+            }
+            else
+            {
+                if (distance < StopFollowDistance) // Less than stop follow distance
+                {
+                    // Reduce velocity by half
+                    rigid.velocity = rigid.velocity / 2;
+                }
+                else 
+                {
+                    rigid.AddForce(Dir * FollowSpeed * (distance / MinFollowDistance));
+                }
+            }
+
+            // Face part to follow
+            transform.LookAt(ObjectToFollow);
+        }
+        else // Look at part to follow
+        {
+            // Face part to follow
+            transform.LookAt(ObjectToFollow);
         }
 
-        // Face part to follow
-        transform.LookAt(PartToFollow);
+        
     }
 }
---
+++ Assets/Scripts/Snake/SnakeManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeManager : MonoBehaviour
+{
+    public GameObject[] SnakePieces;
+
+    public int HealthPerPiece = 3;
+    int Health;
+
+
+    void Start()
+    {
+        // Calc health
+        Health = HealthPerPiece * SnakePieces.Length;
+    }
+
+    public void LoseHealth(int value)
+    {
+        Health -= value;
+
+        if (Health <= 0)
+        {
+            // Snake dead
+        }
+        else if(Health < (SnakePieces.Length - 1 * HealthPerPiece))
+        {
+            if(SnakePieces.Length == 2)
+            {
+                // Flash piece red
+
+                // Destroy last piece
+                DestroyPiece(SnakePieces[SnakePieces.Length - 1]);
+            }
+            else if(SnakePieces.Length == 1)
+            {
+                // Flash piece red
+            }
+            else
+            {
+                // Flash piece red
+
+                // Connect piece (spring) before to piece after
+
+                // Set piece after object to follow to piece before
+
+                // Destroy 2nd to last piece
+                DestroyPiece(SnakePieces[SnakePieces.Length - 2]);
+            }            
+        }
+    }
+
+    void DestroyPiece(GameObject pieceToDestroy)
+    {
+        // Spawn particle effects
+
+        // Destroy piece
+    }
+}
