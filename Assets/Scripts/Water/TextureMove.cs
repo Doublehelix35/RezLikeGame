ef19a0da57caeaa5025cfb7a58d810d7420f9378
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-03-04 10:50:06 +0000

    Added river to egg scene

---
+++ Assets/Scripts/Water/TextureMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureMove : MonoBehaviour
+{
+    public Vector2 MoveDirectionSpeed; // Direction to move texture
+    Vector2 uvOffset = Vector2.zero; // Offset of texture
+
+    string TextureName = "_MainTex";
+    int MatIndex = 0;
+    Renderer rend; // Renderer ref
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        uvOffset += (MoveDirectionSpeed * Time.deltaTime);
+        if (rend.enabled)
+        {
+            rend.materials[MatIndex].SetTextureOffset(TextureName, uvOffset);
+        }
+    }
+}
