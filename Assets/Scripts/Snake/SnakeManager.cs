9ed5ebb7e76c32952ca7d00955aff7247777ae47
Mitchell Dixon <mitchelldixon35@gmail.com>
2020-03-06 16:46:02 +0000

    Snake fixes
    Fixed snake manager to work properly

--- Assets/Scripts/Snake/SnakeManager.cs
+++ Assets/Scripts/Snake/SnakeManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SnakeManager : MonoBehaviour
 {
-    public GameObject[] SnakePieces;
+    public List<GameObject> SnakePieces;
 
     public int HealthPerPiece = 3;
     int Health;
@@ -13,7 +13,7 @@ public class SnakeManager : MonoBehaviour
     void Start()
     {
         // Calc health
-        Health = HealthPerPiece * SnakePieces.Length;
+        Health = HealthPerPiece * SnakePieces.Count;
     }
 
     public void LoseHealth(int value)
@@ -27,35 +27,49 @@ public class SnakeManager : MonoBehaviour
             // Snake dead
             DestroyPiece(gameObject);
         }
-        else if(Health < (SnakePieces.Length - 1 * HealthPerPiece))
+        else
         {
-            if(SnakePieces.Length == 2)
+            // Destroy excess pieces
+            for(int i = SnakePieces.Count; i > 0; i--)
             {
-                Debug.Log("Snake 2 pieces left");
+                // If health less than i * health per piece then remove a piece
+                if(Health < (i * HealthPerPiece) - 3)
+                {
+                    if (SnakePieces.Count == 2)
+                    {
+                        Debug.Log("Snake 2 pieces left");
 
-                // Flash piece red
+                        // Flash piece red
 
-                // Destroy last piece
-                DestroyPiece(SnakePieces[SnakePieces.Length - 1]);
-            }
-            else if(SnakePieces.Length == 1)
-            {
-                Debug.Log("Snake 1 pieces left");
-                // Flash piece red
-            }
-            else
-            {
-                Debug.Log("Snake lost a piece");
+                        // Destroy last piece
+                        DestroyPiece(SnakePieces[SnakePieces.Count - 1]);
+                    }
+                    else
+                    {
+                        Debug.Log("Snake lost a piece");
 
-                // Flash piece red
+                        // Flash piece red
 
-                // Connect piece (spring) before to piece after
+                        // Set last piece's object to follow to piece before removed piece
+                        SnakePieces[SnakePieces.Count - 1].GetComponent<SnakeAi>().ObjectToFollow = SnakePieces[SnakePieces.Count - 3].transform;
 
-                // Set piece after object to follow to piece before
+                        // Move last piece to where removed piece was
+                        Vector3 newPos = SnakePieces[SnakePieces.Count - 2].transform.position;                        
+                        SnakePieces[SnakePieces.Count - 1].transform.position = newPos;
 
-                // Destroy 2nd to last piece
-                DestroyPiece(SnakePieces[SnakePieces.Length - 2]);
-            }            
+                        // Fix spring (connect piece 3rd from end to end piece)
+                        Destroy(SnakePieces[SnakePieces.Count - 3].GetComponent<SpringJoint>()); // Remove spring
+                        SnakePieces[SnakePieces.Count - 3].AddComponent<SpringJoint>(); // Add new spring
+
+                        SnakePieces[SnakePieces.Count - 3].GetComponent<SpringJoint>().connectedBody = SnakePieces[SnakePieces.Count - 1].GetComponent<Rigidbody>();
+
+                        // Destroy 2nd to last piece
+                        DestroyPiece(SnakePieces[SnakePieces.Count - 2]);
+
+                        
+                    }
+                }
+            }                      
         }
     }
 
@@ -64,6 +78,9 @@ public class SnakeManager : MonoBehaviour
         Debug.Log("Destroy this piece: " + pieceToDestroy.name);
 
         // Spawn particle effects
+
+        // Remove from pieces list
+        SnakePieces.Remove(pieceToDestroy);
 
         // Destroy piece
         Destroy(pieceToDestroy);
