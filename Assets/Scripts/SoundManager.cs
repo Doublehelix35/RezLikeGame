671427b31a9053f2f80bb9a9c060e206f2a5918e
JordonPeach <33253642+JordonPeach@users.noreply.github.com>
2020-03-11 11:04:50 +0000

    Soundman

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
@@ -22,8 +23,48 @@ public class SoundManager : MonoBehaviour
 
     public static SoundManager instance = null; //Allows other scripts to call functions from SoundManager
 
+    
+
+    //music for scenes
+    public AudioClip scene1Music; //BeeScene
+    public AudioClip scene2Music; //EggScene
+    public AudioClip scene3Music; //SnakeScene
+    public AudioClip scene4Music; //WinScene
+    
+
+
     public float lowPitchRange = .95f; //Lowest pitch a sfx will be
     public float highPitchRange = 1.05f; //Highest pitch a sfx will be
+
+
+   public static class AudioFadeOut //Call this for music to Fade OUT
+    {
+        public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+        {
+            float startVolume = audioSource.volume;
+            while (audioSource.volume > 0)
+            {
+                audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+                yield return null;
+            }
+            audioSource.Stop();
+        }
+    }
+
+    public static class AudioFadeIn //Call this for Fade In (WIP)
+    {
+        public static IEnumerator FadeIn(AudioSource audioSource, float FadeTime)
+        {
+            audioSource.Play();
+            audioSource.volume = 0f;
+            while (audioSource.volume < 1)
+            {
+                audioSource.volume += Time.deltaTime / FadeTime;
+                yield return null;
+            }
+        }
+    }
+
 
     void Awake()
     {
@@ -90,17 +131,83 @@ public class SoundManager : MonoBehaviour
         efxSource.Play();
     }
 
+    void levelMusicPlay()
+    {
+        Scene CurrentScene = SceneManager.GetActiveScene(); // Gets the current Active Scene
+        Debug.Log(CurrentScene.name);
+        if (CurrentScene.name == "BeeScene")
+        {
+            if (scene1Music != null)
+            {
+                musicSource.clip = scene1Music;
+                musicSource.Play();
+            }
 
+            else
+            {
+                Debug.LogWarning("Music not found for BeeScene Error Code: BeeBeePooPoo");
+            }
+
+        }
+
+        if (CurrentScene.name == "EggScene")
+        {
+            if (scene2Music != null)
+            {
+                musicSource.clip = scene2Music;
+                musicSource.Play();
+            }
+            else
+            {
+                Debug.LogWarning("Music not found for EggScene Error Code: Hard Boiled");
+            }
+        }
+
+        if (CurrentScene.name == "SnakeScene")
+        {
+
+            if(scene3Music != null)
+            {
+                musicSource.clip = scene3Music;
+                musicSource.Play();
+            }
+
+            else
+            {
+                Debug.LogWarning("Music not found for SnakeScene Error Code: Snake in my boot");
+            }
+        }
+
+        if (CurrentScene.name == "WinScene")
+        {
+            if(scene4Music != null)
+            {
+                musicSource.clip = scene4Music;
+                musicSource.Play();
+            }
+            else
+            {
+                Debug.LogWarning("Music not found for WinScene Error Code: did u really win tho?");
+            }
+
+        }
+
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicSource.Play();
+
+        levelMusicPlay();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+
 
 
     }
