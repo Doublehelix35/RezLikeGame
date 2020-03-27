8505762eb89684729e2be5c84e5ad34c563dda1a
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2020-03-27 10:42:33 +0000

    Main Menu

---
+++ Assets/Scripts/Menu/Panel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    public GameObject Panel1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel1.SetActive(false);
+    }
+
+    public void PanelOn()
+    {
+        Panel1.SetActive(true);
+    }
+    public void PanelOff()
+    {
+        Panel1.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu/SceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ToGame()
+    {
+        StartCoroutine(SecenLoading());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator SecenLoading()
+    {
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene("BeeScene");
+    }
+}
