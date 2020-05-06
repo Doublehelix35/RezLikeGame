4552287be63808e448faf9b38b9f871c59ee3a36
JordonPeach <33253642+JordonPeach@users.noreply.github.com>
2020-05-06 11:20:34 +0100

    Menu Buttons Sound Fixed
    All Buttons now make some form of noise

--- Assets/Scripts/Menu/SoundonHover.cs
+++ Assets/Scripts/Menu/SoundonHover.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SoundonHover : MonoBehaviour
 {
     public AudioClip mbutthover;
     public AudioSource Button;
+    public AudioClip gstart;
 
     //script for menu noise
     // Start is called before the first frame update
@@ -14,11 +17,19 @@ public class SoundonHover : MonoBehaviour
         
     }
 
-    private void OnMouseOver()
+    public void OnMouseOver()
     {
         Button.clip = mbutthover;
 
-        Button.PlayOneShot(mbutthover);
+        Button.Play();
+    }
+
+    public void OnMouseDown()
+    {
+        Button.clip = gstart;
+
+        Button.Play();
+
     }
 
     // Update is called once per frame
